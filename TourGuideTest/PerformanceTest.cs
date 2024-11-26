c9977214da2ed3175589e0acf27d067e651adecc
Benoit Podwinski <me@benoitpodwinski.com>
2024-11-26 10:23:47 +0100

    Test error

--- TourGuideTest/PerformanceTest.cs
+++ TourGuideTest/PerformanceTest.cs
@@ -38,7 +38,7 @@ namespace TourGuideTest
 
             _output.WriteLine($"highVolumeTrackLocation ({userCount} users): Time Elapsed: {stopWatch.Elapsed.TotalSeconds} seconds.");
 
-            Assert.True(TimeSpan.FromMinutes(15).TotalSeconds >= stopWatch.Elapsed.TotalSeconds, $"Test failed for {userCount} users.");
+            Assert.False(TimeSpan.FromMinutes(15).TotalSeconds >= stopWatch.Elapsed.TotalSeconds, $"Test failed for {userCount} users.");
         }
 
         [Theory]
